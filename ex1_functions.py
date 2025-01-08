b458a1a20255621ecb6dd75e018b98c38b2e4cb8
LadyJava-script <maayanla88@gmail.com>
2025-01-08 15:51:40 +0200

    Add files via upload

--- ex1_functions.py
+++ ex1_functions.py
@@ -39,28 +39,44 @@ def remove_sparse_columns(df: pd.DataFrame, threshold: int) -> pd.DataFrame:
 def dimensionality_reduction(df: pd.DataFrame, num_components: int, meta_columns: list[str]) -> pd.DataFrame:
     # Separate data into Y(dependent) and X (features)
     y = df[meta_columns]
-    x = df.drop(meta_columns,axis='columns')
-    #normalize the data (for each feature)
+    x = df.drop(meta_columns, axis='columns').fillna(0).copy()
+
+    # normalize the data (for each feature)
     for col in x.columns:
-        x[col] = (x[col] - x[col].mean()) / x[col].std()
-    #convert to numpy array for easier calcs
-    x_np =  x.to_numpy()
-    #calc the features cov (multiply all possible pairs of features by multipling x_np with its transpose and devide by len(df))
-    x_cov = np.cov(x_np.T)
-    #compute egenvectors and egenvalues
+        x[col] = (x[col] - x[col].mean()) / x[col].std(ddof=0)  # שימוש ב-ddof=0 גם בנרמול
+
+    # convert to numpy array for easier calcs
+    x_np = x.to_numpy()
+
+    # חישוב קווריאנס של האוכלוסייה (לא של מדגם)
+    x_cov = np.cov(x_np.T, ddof=0)
+
+    # compute egenvectors and egenvalues
     eigenvalues, eigenvectors = np.linalg.eig(x_cov)
-    #zip, and sort by eigenvalues(x[0]) decending
+
+    # zip, and sort by eigenvalues(x[0]) decending
     eigenvalue_vector_pairs_sorted = sorted(list(zip(eigenvalues, eigenvectors.T)), key=lambda x: x[0], reverse=True)
-    #unzip
+
+    # unzip
     sorted_eigenvalues = np.array([x[0] for x in eigenvalue_vector_pairs_sorted])
     sorted_eigenvectors = np.array([x[1] for x in eigenvalue_vector_pairs_sorted])
+
+    # fix sorted_eigenvectors to abs
+    for i in range(sorted_eigenvectors.shape[0]):
+        if np.max(np.abs(sorted_eigenvectors[i])) != np.max(sorted_eigenvectors[i]):
+            sorted_eigenvectors[i] *= -1
+
     # create matrix P (matrix of eigenvectors) in size num_components
-    P = sorted_eigenvectors[:, :num_components]
-    #calc x_np*P to get projection of the original data onto the new feature space defined by the eigenvectors
+    P = sorted_eigenvectors[:num_components, :].T
+
+    # calc x_np*P to get projection of the original data onto the new feature space defined by the eigenvectors
     new_df = np.dot(x_np, P)
+
     # Convert the projection data into a DataFrame and name columns as PC1, PC2, etc.
-    results_df = pd.DataFrame(data=new_df, columns=[f"PC{i+1}" for i in range(num_components)])
-    #restore meta_columns (dependent values - y)
+    results_df = pd.DataFrame(data=new_df, columns=[f"PC{i + 1}" for i in range(num_components)])
+
+    # restore meta_columns (dependent values - y)
     results_df[meta_columns] = y
+
     return results_df
 
